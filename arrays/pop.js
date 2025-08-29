c13be23f2afa5e6780a3dabee0a42af31f297fd1
SidduKutchula <siddukuchula62@gmail.com>
2025-08-29 10:17:45 +0530

    javascript files

---
+++ arrays/pop.js
@@ -0,0 +1,24 @@
+// using this pop method
+
+//  let  siddu = [1,2,3,4,5];
+//  console.log(siddu);
+//  siddu.pop();
+//  console.log(siddu);
+//  siddu.pop();
+//  console.log(siddu);
+
+
+// using this slice method
+
+// let siddu =[1,2,3,4,5];
+// console.log(siddu);
+// // let mouni = siddu.slice(0,3);
+// let mouni = siddu.slice(1);
+// console.log(mouni)
+// console.log(siddu);
+
+
+let siddu =[1,2,3,4,5];
+console.log(siddu);
+ let mouni = siddu.splice(5,0,6,7,8,9,10);
+ console.log(siddu);
